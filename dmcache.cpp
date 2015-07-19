ac0fa573700da8b84d0a299c9f5e3b3201494b61
Hanh Kieu <hckieu@ucdavis.edu>
2015-07-18 21:19:46 -0700

    converted to bbinary bits wrong but who cares

--- dmcache.cpp
+++ dmcache.cpp
@@ -1,49 +1,58 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 #include <bitset>
 #include <fstream>
 #include <stdlib.h>
+#include <sstream>
+
 
 
 using namespace std;
 
 int main(int argc, char *argv[]){
 	int i, j;
-
-
-
 	/*********initialize memory block/cache line********/
-	string data[32][4];            //data[i][0] = Tag; data[i][1]= Index; [i][2] = offset; [i][3] = data
+	string cache[32][4];            //cache[i][0] = Tag; cache[i][1]= Index; [i][2] = offset; [i][3] = data
 
 	for (i = 0; i < 32; i++){
 		for (j = 0; j < 4; j++){
-			data[i][j] = "0";
+			cache[i][j] = "0";
 		}
 	}
 
 	/**********get data from test file************/
-
-	ifstream fs;
-	
+	ifstream fs;	
 	fs.open(argv[1]);
 	string line;
 	int numberOfLines = 0;
 
+
+
+	string tempString;
+	
+	
 	while(getline(fs, line)){
-		cout << "the line is " << line << endl;
-		++numberOfLines;
+
+		tempString = line.substr(0, line.find(' ')); //cacheline 16 bits
+		stringstream ss;
+		ss << hex << tempString;
+		unsigned n;
+		ss >> n;
+		bitset<16> b(n);
+		cout << b.to_string() << endl;
+		// cout << tempString << endl;
+
+		tempString = (line.substr(line.find(' ') + 1, 2)); //read or write 8 bits
+		// cout << tempString << endl;
+
+		tempString = line.substr(line.rfind(' ') + 1, 2) ;// data 8 bits
+
+		// cout << tempString << endl;
 
 	}
-	
-	string info[numberOfLines][3];   //[i][0] = Address; [i][1] = Read/Write; [i][2] = data
-	
+		
 
-	for (i = 0; i < 300;i++){
-		for (j = 0; j < 3; j++){
-			fs >> info[i][j];
-		}
-	}
-
+	// }
     fs.close();
 
 	/*************cache-write-in**************/
