e7c46bc4a3daede309f55cb1bb07aad560e16fab
Hanh Kieu <hckieu@ucdavis.edu>
2015-07-20 15:30:19 -0700

    rewriting the read just a little almost done

--- dmcache.cpp
+++ dmcache.cpp
@@ -3,16 +3,16 @@
 #include <bitset>
 #include <fstream>
 
-using namespace std;
+
 
 int main(int argc, char *argv[]){
 	int i, j;
 	/*********initialize memory block/cache line********/
 	unsigned int cacheDirty[32];
 	unsigned int cacheTag[32]; //Each cacheline has one tag
-	string cacheData[32][8];//stores our Data, 32 rows, 8 columns because 8 bytes for each cacheline
+	std::string cacheData[32][8];//stores our Data, 32 rows, 8 columns because 8 bytes for each cacheline
 	      //cache[32][0]; stores the tag and cache[32][1] stores if dirty bit or not
-	string memory[0xFFFF]; 	//stores data, it is indexed by the offset we recieve from cacheline
+	std::string memory[0xFFFF][8]; 	//stores data, it is indexed by the offset we recieve from cacheline
 
 
 
@@ -25,91 +25,81 @@ int main(int argc, char *argv[]){
 
 	for (i = 0; i < 32; i++){
 		for (j = 0; j < 8; j++){
-			cacheData[i] = "";
+			cacheData[i][j] = "";
+			memory[i][j] = "";
 		}
 
 	}//initialize all data
 
-	ifstream fs;
+	std::ifstream fs;
 	fs.open(argv[1]);
-	unsigned int offset, index, tag, outputDirty = 0, outputHit = 0;
-	string data;
+	unsigned int offset, index, tag, outputDirty = 0, outputHit = 0, dirty = 0;
+	std::string data;
 	int cacheLine, ops;
-	string outputData;
-
-
+	std::string outputData = "";
 
 	/**********get data from test file/ convert the cachewwwwwwwwwwwwwwwwwwwww line************/
 	
-	
-	
-	
-	
 
 
-	while (fs >> hex >> cacheLine >> ops >> data){
+	while (fs >> std::hex >> cacheLine >> ops >> data){
 
 		offset = cacheLine & 7;
 		index = (cacheLine & 248) >> 3;
 		tag = (cacheLine & 65280) >> 8;
 
 		/*************cache-write-in**************/
+		dirty = cacheDirty[index]; //check to see if dirty
 		if(ops == 255)
 		{
-				cache[index][0] = tag;
-				cache[index][1] = 1; //set it dirty
-					
+			if(cacheTag[index] == tag)
+				cacheData[index][offset] = data;
+			//if hits, put the data where it belongs in cacheline
+			else{
+					if(dirty)
+					{
+						for(int i = 0; i < 8; i++)
+						{
+							 memory[index][i] =  cacheData[index][i] ;
+
+						}//if miss and dirty, transfer cache to memory. Remember to transfer all 8 bytes
+
+					}
+
+					cacheTag[index] = tag;
+					for(int i = 0; i < 8; i++)
+					{
+							   
+						cacheData[index][i] = memory[index][i]; 
+
+					}//if miss and clean, transfer memory to cache, bc cache doesn't contain recent updates
+
+					cacheData[index][offset] = data;
+
+				}
+
+			cacheDirty[index] = 1; //set to dirty
+			
+				
 		}
 		else if(ops == 0)
 		{
 			/*************cache-read-in**************/
-			
-			//checks if hit or miss. If hit then hit = 1, else it misses and hit = 0
 
-			if(cache[index][0] == tag && cache[index][1] == 1){
-					hit = 1;
-					dirty = 1;
-					
-				
-
-					
+			if(cacheTag[index] == tag)
+			{
+				outputHit = 1;
+				outputDirty = cacheDirty[index];
+				outputData = cacheData[index][offset];
 
 
-
-			}//if hit and dirty, then stay dirty
-			else if((cache[index][0] == tag) && (cache[index][1] == 0)){
-					hit = 1;
-					dirty = 0;
+			}//if hits
 			
 
-			}//if hit and clean
-			else if(cache[index][0] != tag && cache[index][1] == 1){
-					hit = 0;
-					dirty = 1;
-					cache[index][1] = 0 ; //clean or dirty
-					cache[index][0] = tag;
 
-					
+		std::cout << " banana " << std::endl;
+		}
 
-
-				
-			}// if miss and dirty
-			else if(cache[index][0] != tag && cache[index][1] == 0){
-					hit = 0;
-					dirty = 0;
-					cache[index][1] = 1;
-					cache[index][0] = tag;
-				
-			}//if miss and clean
-	
-
-			//if dirty and miss then clean it.
-			cout << hex << data;
-			cout << " " << hit << " " << dirty << endl;
-			//checks if dirty bit or not
-
-
-		}	
 	}
 
 	fs.close();
